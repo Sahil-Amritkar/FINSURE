c0b4d3c98f5fadb220201b605bf8087d7032c1ee
Sahil Amritkar <89159821+Sahil-Amritkar@users.noreply.github.com>
2022-09-08 02:30:56 +0530

    finsure login html

--- CppTonightWebsite/finsure/views.py
+++ CppTonightWebsite/finsure/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import hashlib
 
+def check_password():
+    entered_password="Sahil"
+    hashed_password=hashlib.md5(entered_password.encode()).hexdigest()
+    database_password="dc4f69afa57999393ff0988bbdff1181" #use django to find 
+
+    if(entered_password==database_password):
+        return True
+    return False
+
+###
 def finsure_login(request):
-    print(request.GET['your_PAN'])
+    print(request.GET)
     return render(request, 'finsure_login.html', {})
