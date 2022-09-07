cc70fc7e02bd24cf988c664c5fb328e163bdfdc0
Sahil Amritkar <89159821+Sahil-Amritkar@users.noreply.github.com>
2022-09-08 03:42:49 +0530

    changes to views.py

--- CppTonightWebsite/finsure/views.py
+++ CppTonightWebsite/finsure/views.py
@@ -4,7 +4,7 @@ import csv
 
 def check_password(pan, entered_password):
     hashed_password=hashlib.md5(entered_password.encode()).hexdigest()
-    database_password="dc4f69afa57999393ff0988bbdff1181" #use django to find 
+    #database_password="dc4f69afa57999393ff0988bbdff1181" #use django to find 
     file = open('/Users/sahilamritkar/Sahil Codes/Hackathons/Qubit_24hr_Hackathon/FINSURE/CppTonightWebsite/finsure/AAF_Database.csv')
     csvreader = csv.reader(file)
     header = next(csvreader)
@@ -20,13 +20,16 @@ def check_password(pan, entered_password):
 
     db_password=df[pan][1]
 
-    if(hashed_password==database_password):
-        finsure_finances(1)
-    finsure_login(1)
+    if(hashed_password==db_password):
+        return True
+        #finsure_finances(1)
+    return False
+    #finsure_login(1)
 
 ###
 def finsure_login(request):
     result=check_password(request.GET['your_PAN'], request.GET['your_password'])
+    print(result)
     if result==True:
         pass
     else:
