41706e7db3474e27e188dd23d72d155a98e07fed
Sahil Amritkar <89159821+Sahil-Amritkar@users.noreply.github.com>
2022-09-08 04:38:07 +0530

    xyz login implemented

--- CppTonightWebsite/finsure/urls.py
+++ CppTonightWebsite/finsure/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.finsure_login, name='finsure_login'),
     path('', views.finsure_finances, name='finsure_finances'),
     path('', views.finsure_otp, name='finsure_otp'),
+    path('', views.xyz_login, name='xyz_login'),
+    path('', views.xyz_loan, name='xyz_loan'),
+    path('', views.xyz_accepted, name='xyz_accepted'),
+    path('', views.xyz_rejected, name='xyz_rejected'),
 ]
--- CppTonightWebsite/finsure/views.py
+++ CppTonightWebsite/finsure/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render, redirect
 import hashlib
 import csv
 
-def check_password(pan, entered_password):
+def check_password_aaf(pan, entered_password):
     hashed_password=hashlib.md5(entered_password.encode()).hexdigest()
-    #database_password="dc4f69afa57999393ff0988bbdff1181" #use django to find 
     file = open('/Users/sahilamritkar/Sahil Codes/Hackathons/Qubit_24hr_Hackathon/FINSURE/CppTonightWebsite/finsure/AAF_Database.csv')
     csvreader = csv.reader(file)
     header = next(csvreader)
@@ -14,25 +13,41 @@ def check_password(pan, entered_password):
     df={}
     for i in range(len(rows)):
         df[rows[i][0]]=rows[i]
-        # for j in range(len(header)):
-        #     df[rows[i][0][header[j]]]=rows[i][j]
     
-
     db_password=df[pan][1]
 
     if(hashed_password==db_password):
         return True
-        #finsure_finances(1)
     return False
-    #finsure_login(1)
 
-###
+def check_password_xyz(pan, entered_password):
+    hashed_password=hashlib.md5(entered_password.encode()).hexdigest()
+    file = open('/Users/sahilamritkar/Sahil Codes/Hackathons/Qubit_24hr_Hackathon/FINSURE/CppTonightWebsite/finsure/XYZ_Database.csv')
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    rows = []
+    for row in csvreader:
+            rows.append(row)
+    df={}
+    for i in range(len(rows)):
+        df[rows[i][0]]=rows[i]
+    
+    db_password=df[pan][1]
+
+    if(hashed_password==db_password):
+        return True
+    return False
+
+
+
+
+######FUNCTIONS####
 def finsure_login(request):
     result=0
-    result=check_password(request.GET['your_PAN'], request.GET['your_password'])
+    result=check_password_aaf(request.GET['your_PAN'], request.GET['your_password'])
     print(result)
     if result==True:
-        pass
+        return render(request, 'finsure_finances.html', {})
     else:
         pass
         #return to this page
@@ -43,3 +58,23 @@ def finsure_finances(request):
 
 def finsure_otp(request):
     return render(request, 'finsure_otp.html', {})
+
+def xyz_login(request):
+    result=0
+    result=check_password_xyz(request.GET['your_PAN'], request.GET['your_password'])
+    print(result)
+    if result==True:
+        return render(request, 'xyz_loan.html', {})
+    else:
+        pass
+        #return to this page
+    return render(request, 'xyz_login.html', {})
+
+def xyz_loan(request):
+    return render(request, 'xyz_loan.html', {})
+
+def xyz_accepted(request):
+    return render(request, 'xyz_accepted.html', {})
+
+def xyz_rejected(request):
+    return render(request, 'xyz_rejected.html', {})
